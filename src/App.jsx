b0c186b712212a0d98ac71420bec22e6a6d829e8
Pradeep Khatri <pradeepkhatri44@live.com>
2025-04-18 17:10:28 +0530

    commiting Edusity college USA
    commiting Edusity college USA

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import Programs from "./Components/programs/programs";
+import Title from "./Components/Title/Title";
+import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import VideoPlayer from "./Components/Video_Player/VideoPlayer";
+
+const App = () => {
+
+  const [playState, setPlayState] = useState(false);
+
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle="Our PROGRAM" title="What We Offer" />
+        <Programs />
+        <About setPlayState={setPlayState}/>
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
+        <Title subTitle="TESTIMONIALS" title="What students say" />
+        <Testimonials />
+        <Title subTitle="Contact Us" title="Get in touch" />
+        <Contact/>
+        <Footer/>
+      </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>
+    </div>
+  );
+};
+
+export default App;
